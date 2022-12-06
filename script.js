17aea1e4ca047ae16e95873bfdf084698b395748
jagmitswami <jagmitswami@gmail.com>
2022-12-06 12:50:50 +0530

    added

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let togglebutton = document.getElementsByClassName('totoggle')[0];
 let navlink = document.getElementsByClassName('subnav')[0];
+let flagIntro = true;
 togglebutton.addEventListener("click",function(){
     navlink.classList.toggle('active');
+    if(flagIntro==true){
+        document.querySelector('.bodyy').style.paddingTop='250px';
+        flagIntro = false;
+    }else{
+        document.querySelector('.bodyy').style.paddingTop='10px';
+        flagIntro = true;
+    }
 });
