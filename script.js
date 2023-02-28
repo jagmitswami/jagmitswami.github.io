14c0711796c16d635be4936ec74720884deaaac7
Jagmit <bitscotch@gmail.com>
2023-02-28 14:02:45 +0530

    updated portfolio

--- script.js
+++ script.js
@@ -23,3 +23,7 @@ scrollServices.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     scrollServices.scrollLeft += evt.deltaY;
 });
+
+function openResume(){
+    window.open("https://drive.google.com/file/d/10Pbz_sipt-qR3D8dyCI8dGdtBWOjIl1z/view?usp=sharing", "_blank")
+}
